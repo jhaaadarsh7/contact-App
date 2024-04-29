7db9ab1c7f3e83870426f6a3f7cb66dee8dcdb04
jhaaadarsh7 <jhaaadarsh582@gamail.com>
2024-04-29 19:31:37 +0545

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,97 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import { FiSearch } from "react-icons/fi";
-import { IoIosAddCircleOutline } from "react-icons/io";
-import { collection, getDocs } from "firebase/firestore"; // Removed unnecessary import
+import { IoIosAddCircleOutline, IoMdTrash } from "react-icons/io";
+import { collection, getDocs, onSnapshot } from "firebase/firestore"; // Removed unnecessary import
 import { db } from "./config/firebase";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ContactCard from "./components/ContactCard";
+import AddAndUpdateContact from "./components/AddAndUpdateContact";
+import NotFoundContact from "./components/NotFoundContact";
+// import Modal from "./components/Modal";
+import useDisclouse from "./Hooks/useDisclouse";
 const App = () => {
   const [contacts, setContacts] = useState([]);
+
+  const { isOpen, onClose, onOpen } = useDisclouse();
 
   useEffect(() => {
     const getContacts = async () => {
       try {
-        const contactRef = collection(db, "contacts");
-        const contactsSnapShot = await getDocs(contactRef);
-        const contactLists = contactsSnapShot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        setContacts(contactLists);
+        const contactsRef = collection(db, "contacts");
+
+        onSnapshot(contactsRef, (snapshot) => {
+          const contactLists = snapshot.docs.map((doc) => {
+            return {
+              id: doc.id,
+              ...doc.data(),
+            };
+          });
+          setContacts(contactLists);
+          return contactLists;
+        });
       } catch (error) {
         console.log(error);
       }
     };
 
     getContacts();
-  }, []); // Empty dependency array to run the effect only once
+  }, []);
+  const filterContacts = (e) => {
+    const value = e.target.value;
 
+    const contactsRef = collection(db, "contacts");
+
+    onSnapshot(contactsRef, (snapshot) => {
+      const contactLists = snapshot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
+
+      const filteredContacts = contactLists.filter((contact) =>
+        contact.name.toLowerCase().includes(value.toLowerCase())
+      );
+
+      setContacts(filteredContacts);
+
+      return filteredContacts;
+    });
+  };
   return (
-    <div className="mx_auto max-w-[370px] px-4">
-      <Navbar />
-      <div className="flex">
-        <div className="relative flex flex-grow items-center">
-          <FiSearch className="ml-1 absolute text-3xl text-white" />
-          <input
-            type="text"
-            className="h-10 flex-grow rounded-md border border-white bg-transparent text-white pl-9"
+    <>
+      <div className="mx_auto max-w-[370px] px-4">
+        <Navbar />
+        <div className="flex gap-2">
+          <div className="relative flex flex-grow items-center">
+            <FiSearch className="ml-1 absolute text-3xl text-white" />
+            <input
+              onChange={filterContacts}
+              type="text"
+              className="h-10 flex-grow rounded-md border border-white bg-transparent text-white pl-9"
+            />
+          </div>
+          <IoIosAddCircleOutline
+            onClick={onOpen}
+            className="cursor-pointer text-5xl text-white"
           />
         </div>
-        <IoIosAddCircleOutline className="cursor-pointer text-5xl text-white" />
+        <div className="mt-4 flex flex-col gap-3">
+          {contacts.length <= 0 ? (
+            <NotFoundContact />
+          ) : (
+            contacts.map((contact) => (
+              <ContactCard key={contact.id} contact={contact} />
+            ))
+          )}
+        </div>
       </div>
-    </div>
+
+      <AddAndUpdateContact onClose={onClose} isOpen={isOpen} />
+      <ToastContainer position="bottom-center" />
+    </>
   );
 };
 
